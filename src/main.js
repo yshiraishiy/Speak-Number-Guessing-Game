c13f84c69f708465625f8f3067a0c5c80a103bba
yshiraishiy <yuya0505soccer@gmail.com>
2025-01-15 14:55:00 +0900

    ✨推測された番号を確認する

--- src/main.js
+++ src/main.js
@@ -10,14 +10,50 @@ window.SpeechRecognition =
 let recognition = new window.SpeechRecognition();
 
 // 認識とゲームをスタート
-// recognition.start();
+recognition.start();
 
 // ユーザーの音声をキャプチャ
 function onSpeak(e) {
   const msg = e.results[0][0].transcript;
 
-  // writeMessage(msg)
-  // checkNumber(msg)
+  writeMessage(msg);
+  checkNumber(msg);
+}
+
+// ユーザーが話した内容を追記
+function writeMessage(msg) {
+  msgEl.innerHTML = `
+    <div>You said: </div>
+    <span class="box">${msg}</span>
+  `;
+}
+
+// 'msg'が数値か確認
+function checkNumber(msg) {
+  const num = +msg;
+
+  if (Number.isNaN(num)) {
+    msgEl.innerHTML += "<div>That is not a valid number</div>";
+    return;
+  }
+
+  // 数値が範囲内か確認
+  if (num > 100 || num < 1) {
+    msgEl.innerHTML += "<div>Number must be between 1 and 100</div>";
+    return;
+  }
+
+  // 数値を確認
+  if (num === randomNum) {
+    document.body.innerHTML = `
+      <h2>Congrats! You have guessed the number! <br><br>It was ${num}</h2>
+      <button class="play-again" id="play-again">Play Again</button>
+    `;
+  } else if (num > randomNum) {
+    msgEl.innerHTML += "<div>GO LOWER</div>";
+  } else {
+    msgEl.innerHTML += "<div>GO HIGHER</div>";
+  }
 }
 
 // ランダムな数字を作成
@@ -27,3 +63,14 @@ function getRandomNumber() {
 
 // 音声を話す
 recognition.addEventListener("result", onSpeak);
+
+// 音声認識を終了したら再度スタート
+recognition.addEventListener("end", () => {
+  recognition.start();
+});
+
+document.body.addEventListener("click", (e) => {
+  if (e.target.id === "play-again") {
+    window.location.reload();
+  }
+});
